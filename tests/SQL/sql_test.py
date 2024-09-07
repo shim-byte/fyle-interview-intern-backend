630ea4594c34432d969ed633d1b2dd3dcc1db539
Mohtashim Ansari <mohtashim.in@gmail.com>
2024-09-08 00:31:35 +0530

    Fixed buggy test cases and added Docker yml

--- tests/SQL/sql_test.py
+++ tests/SQL/sql_test.py
@@ -66,7 +66,7 @@ def test_get_assignments_in_graded_state_for_each_student():
     db.session.commit()
 
     # Define the expected result before any changes
-    expected_result = [(1, 3)]
+    expected_result = [(2, 3)]
 
     # Execute the SQL query and compare the result with the expected result
     with open('tests/SQL/number_of_graded_assignments_for_each_student.sql', encoding='utf8') as fo:
--- tests/principals_test.py
+++ tests/principals_test.py
@@ -60,3 +60,18 @@ def test_regrade_assignment(client, h_principal):
 
     assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
     assert response.json['data']['grade'] == GradeEnum.B
+
+
+
+def test_list_all_teachers(client, h_principal):
+    response = client.get('/principal/teachers', headers=h_principal)
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    assert isinstance(data, list)
+    for teacher in data:
+        assert 'id' in teacher
+        assert 'user_id' in teacher
+        assert 'created_at' in teacher
+        assert 'updated_at' in teacher
